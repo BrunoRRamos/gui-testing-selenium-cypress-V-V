23f22fce05cdf1c2dfd9765c593133beb7421237
BrunoRamos <bruno.ramos@ccc.ufcg.edu.br>
2025-03-22 18:16:15 -0300

    test: Testes cypress

--- cypress/e2e/customers.cy.js
+++ cypress/e2e/customers.cy.js
@@ -4,31 +4,71 @@ describe('customers', () => {
     cy.get('[id="_username"]').type('sylius');
     cy.get('[id="_password"]').type('sylius');
     cy.get('.primary').click();
-  });
-  // Remove .only and implement others test cases!
-  it.only('update a customer phone number', () => {
-    // Click in customers in side menu
     cy.clickInFirst('a[href="/admin/customers/"]');
-    // Type in value input to search for specify customer
+  });
+
+  // Justificativa: Se fez necessario gerar nomes de email aleatorios
+  // pois o sylius sempre mantem registro de guests para controle
+  // e para que o teste de criar customer funcione deve ser 
+  // utilizado um email que não esteja em uso.
+  function genEmail() {
+    const randomStr = Math.random().toString(36).substring(2, 8);
+    return `${randomStr}`;
+  }
+
+  it('1- Update a customer phone number', () => {
     cy.get('[id="criteria_search_value"]').type('@gmail');
-    // Click in filter blue button
     cy.get('*[class^="ui blue labeled icon button"]').click();
-    // Click in edit of the last customer
     cy.get('*[class^="ui labeled icon button "]').last().click();
-    // Edit customer phone number
     cy.get('[id="sylius_customer_phoneNumber"]').scrollIntoView().clear().type('999.999.9999');
-    // Click on Save changes button
     cy.get('[id="sylius_save_changes_button"]').scrollIntoView().click();
 
-    // Assert that customer has been updated
     cy.get('body').should('contain', 'Customer has been successfully updated.');
   });
-  it('test case 2', () => {
-    // Implement your test case 2 code here
-  });
-  it('test case 3', () => {
-    // Implement your test case 3 code here
+
+  it('2- Create new customer', () => {
+    cy.findAllByText(/^Create/i).first().click();
+    cy.get('#sylius_customer_firstName').type("Jobson");
+    cy.get('#sylius_customer_lastName').type("Silva");
+    cy.get('#sylius_customer_email').type(`${genEmail()}@gmail.com`);
+    cy.get('#sylius_customer_group').select("Retail");
+    cy.get('#sylius_customer_gender').select("Male");
+    cy.get('#sylius_customer_birthday').type("1987-12-12");
+    cy.get('#sylius_customer_phoneNumber').type("99999999");
+    cy.findAllByText(/^Create/i).first().click();
+
+    cy.get('body').should('contain', 'Customer has been successfully created.');
   });
 
-  // Implement the remaining test cases in a similar manner
+  it('3- Search for a user', () => {
+    cy.get('#criteria_search_type').select("equal");
+    cy.get('#criteria_search_value').type("Jobson");
+    cy.get('.sylius-autocomplete').type("Retail{enter}");
+    cy.findAllByText(/^Filter/i).first().click();
+
+    cy.findAllByText(/^Jobson/i).should("be.visible");
+    cy.findAllByText(/^Silva/i).should("be.visible");
+  });
+
+  it('4- Grant account credentials', () => {
+    cy.get('.segment').scrollTo("right");
+    cy.findAllByText(/^Edit/i).first().click();
+    cy.findAllByText(/^Customer can login to the store?/i).click();
+    cy.get('#sylius_customer_user_plainPassword').type("pass123");
+    cy.findAllByText(/^Enabled/i).click();
+    cy.findAllByText(/^Verified/i).click();
+    cy.findAllByText(/^Save changes/i).first().click();
+
+    cy.get('body').should('contain', 'Customer has been successfully updated.');
+  });
+
+  it('5- Delete custumer', () => {
+    cy.get('.segment').scrollTo("right");
+    cy.findAllByText(/^Show/i).eq(2).click();
+    cy.findAllByText(/^Jobson Silva/i).should("be.visible");
+    cy.findAllByText(/^Delete/i).first().click();
+    cy.findAllByText(/^Yes/i).first().click();
+
+    cy.get('body').should('contain', 'Shop user has been successfully deleted.');
+  });
 });
